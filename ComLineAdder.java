2065ac7ef01a8b1d2585a17c65e45d9c1ec78fda
Tia Bishop <tbishop2@xula.edu>
2021-09-29 14:30:38 +0000

    catches non valid arguement

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -1,8 +1,16 @@
 public class ComLineAdder {
     public static void main(String[] args) {
+        if(args.length < 1){
+            System.err.println("Must enter number on command line");
+        }
         int sum = 0;
         for(int ndx = 0; ndx < args.length; ndx++) {
-            sum += Integer.parseInt(args[ndx]);
+            try{
+               sum += Integer.parseInt(args[ndx]); 
+            }catch(Exception e){
+                System.err.printf("Cannot convert %s\n", args[ndx]);
+            }
+            
         }
         System.out.printf("Sum of %d values is: %,d\n",args.length,sum);
     }
