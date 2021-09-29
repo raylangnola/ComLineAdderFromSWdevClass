1362dd18cb83059dccd998ae4afb71e200bab8ba
Student B Test <student+xavier-university-of-louisiana2@codio.com>
2021-09-29 14:30:12 +0000

    catches invalid args

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -2,7 +2,11 @@ public class ComLineAdder {
     public static void main(String[] args) {
         int sum = 0;
         for(int ndx = 0; ndx < args.length; ndx++) {
-            sum += Integer.parseInt(args[ndx]);
+            try {
+                sum += Integer.parseInt(args[ndx]);
+            } catch (Exception e) {
+                System.out.printf("Couldn't convert %s\n",args[ndx]);
+            }       
         }
         System.out.printf("Sum of %d values is: %,d\n",args.length,sum);
     }
