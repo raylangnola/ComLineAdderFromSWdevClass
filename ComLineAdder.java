eac0d6c5a9a8b2c0a948159e7ce4fe7e9cb20f3c
Alexus Allen <aallen31@xula.edu>
2021-09-30 02:10:34 +0000

    Create ArrayList that stores values and computes the sum.

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -1,13 +1,19 @@
+import java.util.*;
+
 public class ComLineAdder {
     public static void main(String[] args) {
         int sum = 0;
+        List<Integer> total = new ArrayList<>();
         for(int ndx = 0; ndx < args.length; ndx++) {
             try {
-                sum += Integer.parseInt(args[ndx]);
+                total.add(Integer.parseInt(args[ndx]));
             } catch (Exception e) {
                 System.out.printf("Couldn't convert %s\n",args[ndx]);
             }       
         }
+        for(int num : total){
+                   sum += num;
+               }
         System.out.printf("Sum of %d values is: %,d\n",args.length,sum);
     }
 }
