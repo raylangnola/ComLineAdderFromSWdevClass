dff7e6065ad28453e71311bde69830fd2eb5c65c
Tia Bishop <tbishop2@xula.edu>
2021-09-30 19:55:14 +0000

    Working add sums with arraylist

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -7,24 +7,15 @@ public class ComLineAdder {
             System.err.println("Must enter number on command line");
         }
         int sum = 0;
-        //int [] totalSum = 0;
         List<Integer> listOfNums = new ArrayList<>();
-        //System.out.println("These are the number: "+ numbers);
-        //System.out.println("These are the args: "+ args[0]);
-        
+
         for(int ndx = 0; ndx < args.length; ndx++) {
             try{
-               //listOfNums.add(Integer.parseInt(args.get(ndx)));  // keeps printing out list at every iteration
-               // index
                 int num = Integer.parseInt(args[ndx]);
                 listOfNums.add(num);
-               
-               //sum += Integer.parseInt(args[ndx]); // prints out sum
             }catch(Exception e){
                 System.err.printf("Cannot convert %s\n", args[ndx]);
             }
-            
-       
         }
         System.out.println("This is the list" + listOfNums);
          
@@ -32,13 +23,10 @@ public class ComLineAdder {
                  sum += number;
              }
 
-        //double mean = sum / listOfNums.size();// calculates average
+        double mean = sum / listOfNums.size();// calculates average
         System.out.printf("Sum of %d values is: %,d\n",args.length,sum);
-        //System.out.printf("Average of values is: %,.2f\n",mean);
+        System.out.printf("Average of values is: %,.2f\n",mean);
     }// main
-//     public void getListSum(){
-        
-//     }
 }// class
 
  
