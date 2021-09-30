cbf28826990c91eca445f267aee47e3970db0d8d
Zari Hamilton <zhamilto@xula.edu>
2021-09-30 02:24:06 +0000

    New change

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -1,9 +1,21 @@
+import java.util.*;
 public class ComLineAdder {
     public static void main(String[] args) {
         int sum = 0;
+        List<Integer> amount = new ArrayList<>();
         for(int ndx = 0; ndx < args.length; ndx++) {
-            sum += Integer.parseInt(args[ndx]);
+            try{
+                amount.add(Integer.parseInt(args[ndx]));
+               
+        }catch (Exception e){
+                System.out.printf("Couldn't convert %s\n" ,args[ndx]);
+            }
         }
+        for(int num: amount){
+                   sum += num ;
+
+                    
+                }
         System.out.printf("Sum of %d values is: %,d\n",args.length,sum);
     }
 }
