ebddff0dab9d408f02361217a6641cbb2cb01d12
apalmer778 <apalmer5@xula.edu>
2021-09-30 23:50:28 -0500

    stored cmd line args in int array

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -1,12 +1,25 @@
+package ComLineAdderFromSWdevClass;
+
+
 public class ComLineAdder {
     public static void main(String[] args) {
+        Integer[] values = new Integer[args.length];
+        int index = 0;
+        for (String str : args){
+            try{
+                values[index] = Integer.parseInt(str);
+                index++;
+            }catch(Exception e){
+                System.out.printf("Couldn't convert %s\n",args[index]);
+                index -= 1;
+            }
+        }
         int sum = 0;
-        for(int ndx = 0; ndx < args.length; ndx++) {
-            try {
-                sum += Integer.parseInt(args[ndx]);
-            } catch (Exception e) {
-                System.out.printf("Couldn't convert %s\n",args[ndx]);
-            }       
+        for(int ndx = 0; ndx < values.length; ndx++) {
+            if (values[ndx]==null){
+                continue;
+            }
+            sum += values[ndx];
         }
         System.out.printf("Sum of %d values is: %,d\n",args.length,sum);
     }
