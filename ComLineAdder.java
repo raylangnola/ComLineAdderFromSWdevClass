b544c7c447f60d01a9070c49f9e424d3ce86de99
Fernando Llopis <fquereda@xula.edu>
2021-10-01 02:22:20 +0000

    fixed value cont

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -1,15 +1,23 @@
+import java.util.ArrayList;
+
 public class ComLineAdder {
     public static void main(String[] args) {
         int sum = 0;
         int count = 0;
+        double average = 0;
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
         for(int ndx = 0; ndx < args.length; ndx++) {
             try {
-                sum += Integer.parseInt(args[ndx]);
+                numbers.add(Integer.parseInt(args[ndx]));
                 count++;  
             } catch (Exception e) {
                 System.out.printf("Couldn't convert %s\n",args[ndx]);
-            }       
+            }   
         }
-        System.out.printf("%d arguments, sum of %d values is: %,d\n",args.length,count,sum);
+        for(int j = 0; j < numbers.size(); j++){
+            sum += numbers.get(j);
+            average = sum/numbers.size();
+        }
+        System.out.printf("%d arguments, %1.2f average, sum of %d values is: %,d\n",args.length,average,count,sum);
     }
 }
