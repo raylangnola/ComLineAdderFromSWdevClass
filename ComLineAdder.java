41782e4b4431729be79c7ed0e9658f15e3572e35
Abryhia Irons <airons@xula.edu>
2021-10-01 01:12:48 +0000

    fixed value count

--- ComLineAdder.java
+++ ComLineAdder.java
@@ -1,15 +1,27 @@
+import java.util.*;
 public class ComLineAdder {
     public static void main(String[] args) {
         int sum = 0;
         int count = 0;
+        List<Integer> intAmount = new ArrayList<Integer>();
+        
         for(int ndx = 0; ndx < args.length; ndx++) {
             try {
                 sum += Integer.parseInt(args[ndx]);
+                int i = Integer.parseInt(args[ndx]);
+                intAmount.add(i);
                 count++;
             } catch (Exception e) {
                 System.out.printf("Couldn't convert %s\n",args[ndx]);
-            }       
+                
+            }  
+        }
+        for (int i: intAmount){
+            sum += i;
         }
         System.out.printf("%d arguments, sum of %d values is: %,d\n",args.length,count,sum);
     }
 }
+
+/*Couldnt figure out how i can get it to count a string in the  
+ amount of values that are entered but it adds up into the sum*/
